3c8c4db9684241c9e17da898091d43cfbddc34f4
Fabio Giaquinta <extern.fabio.giaquinta@europcar.com>
2024-11-20 11:20:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# vue-crash-course-2024
+
+[YT tutorial](https://www.youtube.com/watch?v=VeNfHj6MhgA)
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vite.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+import HomeView from "@/view/HomeView.vue"
+import JobsView from "@/view/JobsView.vue"
+import NotFoundView from "@/view/NotFoundView.vue"
+import JobView from "@/view/JobView.vue"
+import AddJobView from "@/view/AddJobView.vue"
+import EditJobView from "@/view/EditJobView.vue"
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/jobs',
+            name: 'jobs',
+            component: JobsView
+        },
+        {
+            path: '/jobs/:id',
+            name: 'job',
+            component: JobView
+        },
+        {
+            path: '/jobs/add',
+            name: 'add-job',
+            component: AddJobView
+        },
+        {
+            path: '/jobs/edit/:id',
+            name: 'edit-job',
+            component: EditJobView
+        },
+        {
+            path: '/:catchAll(.*)',
+            name: 'notFound',
+            component: NotFoundView
+        }
+    ]
+})
+
+export default router
